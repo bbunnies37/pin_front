7ba1e1fab8930da43be7b5beff75312b722d7343
Christine Chaewon Shin <christine@Christines-MacBook-Air.local>
2023-02-10 22:49:52 +0900

    [init] create new vue project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="h-screen">
+    <div class="h-1/6 bg-slate-600 text-white">페이지 헤더 영역</div>
+    <RouterView class="h-5/6"></RouterView>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
---
+++ src/views/Landing.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-slate-300">
+    <h1>landing page</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
