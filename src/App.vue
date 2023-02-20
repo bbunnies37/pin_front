13f72e31d4fd9e604aa654603d9d669427ccee09
박수아 <xlalfskdl@naver.com>
2023-02-20 23:51:56 +0900

    [style] Home.vue page and Pin.vue publish

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
     <div class="h-1/6 bg-slate-600 text-white">
       페이지 헤더 영역
       <router-link to="/pin-builder">create pin</router-link>
+      <br/>
+      <router-link to="/home">Home</router-link>
+      <br/>
+      <router-link to="/pin-detail">pin detail</router-link>
     </div>
     <router-view></router-view>
   </div>
