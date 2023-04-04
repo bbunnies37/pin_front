db94be13a604b5de33beba2b875a2abace7f308e
Christine Chaewon Shin <chshin0225@gmail.com>
2023-04-04 23:41:02 +0900

    [style]: create Settings.vue

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -3,7 +3,7 @@
     <!-- 설정 사이드 탭 -->
     <div class="w-[207px]">
       <div class="mb-2 w-[90px]">
-        <a @click="">
+        <a @click="openTab('publicProfile')">
           <div class="p-2 cursor-pointer hover:bg-secondary rounded-lg">
             <div>
               공개 프로필
@@ -13,7 +13,7 @@
         </a>
       </div>
       <div class="mb-2 w-[90px] rounded-lg">
-        <a @click="">
+        <a @click="openTab('homeFeed')">
           <div class="p-2 cursor-pointer hover:bg-secondary rounded-lg">
             <div>
               홈피드 조정
@@ -26,7 +26,7 @@
 
     <div class="w-[488px]">
       <!-- 공개 프로필 -->
-      <div>
+      <div id="publicProfile" class="hidden tabcontent">
         <div>
           <h1 class="font-semibold text-[28px] mb-2">
             공개 프로필
@@ -85,15 +85,102 @@
       </div>
 
       <!-- 홈피드 조정 -->
-      <div>
+      <div id="homeFeed" class="hidden tabcontent">
+        <div>
+          <h1 class="font-semibold text-[28px] mb-2">
+            홈피드 조정
+          </h1>
+          <div>
+            Pinterest를 자신에 맞게 <i>만들어보세요.</i> Pinterest가 아이디어를 추천하기 위<br>
+            해 사용하는 세부 정보를 편집해 보세요. 본 세부 정보는 공개적으로 표시되지<br>
+            않습니다.
+          </div>
+        </div>
+        <div class="py-4">
+          <div class="flex">
+            <div class="mx-2">
+              <div class="py-3">
+                <div class="p-2 font-semibold rounded-lg hover:bg-secondary cursor-pointer" @click="openSubTab('interests')">
+                  관심사
+                </div>
+              </div>
+            </div>
+            <div class="mx-2">
+              <div class="py-3">
+                <div class="p-2 font-semibold rounded-lg hover:bg-secondary cursor-pointer" @click="openSubTab('boards')">
+                  보드
+                </div>
+              </div>
+            </div>
+            <div class="mx-2">
+              <div class="py-3">
+                <div class="p-2 font-semibold rounded-lg hover:bg-secondary cursor-pointer" @click="openSubTab('following')">
+                  팔로잉
+                </div>
+              </div>
+            </div>
+          </div>
 
+          <!-- 관심사 탭 -->
+          <div id="interests" class="py-1 hidden sub-tab-content">
+            <div class="mb-3 text-gray-500">
+              관심사를 추가하고 관련 아이디어를 확인해 보세요. 관심사를 삭제하면 더 이상<br>
+              관심이 없는 관련 아이디어가 표시되지 않습니다.
+            </div>
+          </div>
+
+          <!-- 보드 탭 -->
+          <div id="boards" class="py-1 hidden sub-tab-content">
+            <div class="mb-3 text-gray-500">
+              이와 관련된 아이디어를 더 이상 보고 싶지 않으면 보드를 끄세요. 걱정마세요.<br>
+              보드에는 아무런 영향을 미치지 않습니다.
+            </div>
+          </div>
+          
+          <!-- 팔로잉 탭 -->
+          <div id="following" class="py-1 hidden sub-tab-content">
+            <div class="mb-3 text-gray-500">
+              게시된 핀을 더 이상 보지 않으려면 해당 개인이나 브랜드를 언팔로우하세요.<br>
+              이들에게는 통지되지 않습니다.
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+  import { ref, onMounted } from 'vue'
 
+  const tab = ref('tab1')
+
+  const openTab = (menu: string) => {
+    // Get all elements with class="tabcontent" and hide them
+    const tabcontent = document.getElementsByClassName('tabcontent') as HTMLCollectionOf<HTMLElement>
+    for (let i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = 'none'
+    }
+
+    const tab = document.getElementById(menu)
+    if (tab) tab.style.display = 'block'
+  }
+
+  const openSubTab = (menu: string) => {
+    // Get all elements with class="sub-tab-content" and hide them
+    const tabcontent = document.getElementsByClassName('sub-tab-content') as HTMLCollectionOf<HTMLElement>
+    for (let i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = 'none'
+    }
+
+    const subTab = document.getElementById(menu)
+    if (subTab) subTab.style.display = 'block'
+  }
+
+  onMounted(() => {
+    openTab('publicProfile')
+    openSubTab('interests')
+  })
 </script>
 
 <style scoped>
